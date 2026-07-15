fdc7732b9cba9dbd833c44b0be36a9ffad73896b
Farianskiy <mr.kosmonat@mail.ru>
2026-07-15 20:31:18 +0300

    Create initial FocusFlow project structure

---
+++ src/FocusFlow.Mobile/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace FocusFlow.Mobile;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ src/FocusFlow.Mobile/Platforms/Android/MainApplication.cs
@@ -0,0 +1,15 @@
+﻿using Android.App;
+using Android.Runtime;
+
+namespace FocusFlow.Mobile;
+
+[Application]
+public class MainApplication : MauiApplication
+{
+	public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+		: base(handle, ownership)
+	{
+	}
+
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
